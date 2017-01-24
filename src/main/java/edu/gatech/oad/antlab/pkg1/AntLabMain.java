4bbdefa78168732d991ba61fac034354a84209b3
tesorrells <trevor.sorrells45@gmail.com>
2017-01-24 08:50:42 -0500

    added my name
    added name and gtID

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -61,10 +61,8 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //and gburdell with your gt id
 		  Person2 p2 = new Person2("P2");
 		  toPrint += p2.toString("gburdell2");
-		  //Person3 replace P3 with your name
-		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("P3");
-		  toPrint += p3.toString("gburdell3");
+		  Person3 p3 = new Person3("Trevor Sorrells");
+		  toPrint += p3.toString("tsorrells7");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("P4");
