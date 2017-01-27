966781049c12755db16b3debb5a6550f50c77bfa
david baker <davidjbaker1994@gmail.com>
2017-01-27 15:04:17 -0500

    added Person5

--- src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -3,9 +3,9 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 5
  *  returns their name and a
- *  modified string 
- *  
- *  @author Bob
+ *  modified string
+ *
+ *  @author Jake Baker
  *  @version 1.1
  */
 public class Person5 {
@@ -30,20 +30,30 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+        if (input.length() > 0) {
+            String newString = input.substring(input.length() - 2,
+	  	        input.length()) + input.substring(0, input.length() - 2);
+        } else {
+        	newString = input;
+        }
+
+        return newString;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
 	  return name + calc(input);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(calc("Jake"));
 	}
 
 }
